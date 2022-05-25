4c0fdfe399b0580678d7bbd115feeed80a97c05c
Виталий Спирин <vitalik.direkt@yandex.ru>
2022-05-25 15:12:51 +0300

    Initial Commit

--- homework.py
+++ homework.py
@@ -1,9 +1,19 @@
 import datetime as dt
 
 
-class Calculator:
-    pass
+date = dt.date.today()
+print(date)
 
-
-class Record:
-    pass
+#
+#
+# class Calculator:
+#     pasы
+#
+#
+# class Record:
+#     """ Создает записи о:
+#         1. денежной сумме/калориях
+#         2. комментарий на что потрачены/как получены
+#         3. дата создания записи"""
+#
+#     def __init__(self, amount: float, comment: str, date: Optional(date)) --> Record:
